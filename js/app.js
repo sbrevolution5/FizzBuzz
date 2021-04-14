31ce3e006b9a431d53663d1b2c087c5e714d4174
sbrevolution5 <sbrevolution5@aim.com>
2021-04-14 15:21:15 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,73 @@
+function main() {
+    let fizz = parseInt(document.getElementById("fizz").value);
+    let buzz = parseInt(document.getElementById("buzz").value);
+    let limit = parseInt(document.getElementById("limit").value);
+    let errorText = document.getElementById("errorText")
+    if (limit > 50000) {
+        errorText.textContent = "Error: Enter a limit below 50,000"
+
+    } else if (limit <= 0) {
+        errorText.textContent = "Error: limit must be positive"
+    } else if (isNaN(fizz) || isNaN(buzz) || isNaN(limit)) {
+        errorText.textContent = "Error: one of your inputs isn't a number"
+    } else {
+        errorText.textContent = ""
+    }
+    //gets numbers
+    let numList = getList(limit)
+    //replace with fizz/buzz/fizzbuzz 
+    let fizzed = fizzbuzz(fizz, buzz, numList)
+    // console.log(fizzed)
+
+    // displays array
+    // display(fizzed)
+}
+
+function getList(limit) {
+    let res = []
+    for (let i = 1; i <= limit; i++) {
+        res.push(i);
+
+    }
+    return res;
+}
+
+function fizzbuzz(fizz, buzz, list) {
+    let res = [];
+    for (let i = 0; i < list.length; i++) {
+        if (list[i] % fizz == 0 && list[i] % buzz == 0) {
+            res.push("FizzBuzz")
+        } else if (list[i] % fizz == 0) {
+            res.push("Fizz")
+        } else if (list[i] % buzz == 0) {
+            res.push("Buzz")
+        } else {
+            res.push(`${list[i]}`)
+        }
+    }
+    return res
+}
+
+function display(fbList) {
+    const fbTemplate = document.getElementById("fbTemplate")
+    const resultsBody = document.getElementById("resultsBody")
+    let colCount = fbTemplate.content.cloneNode(true).querySelectorAll("td").length;
+    // clears table to insert new data
+    resultsBody.innerHTML = "";
+    // loops over every element in the numbers array by row
+    for (let i = 0; i < numbers.length; i += colCount) {
+        let dataRow = fbTemplate.content.cloneNode(true);
+        let cols = dataRow.querySelectorAll("td");
+        for (let colI = 0; colI < cols.length; colI++) {
+            let value = numbers[i + colI]
+            if (typeof value === "undefined") {
+                value = ""
+            } else if (value % 2 == 0) {
+                cols[colI].classList.add("boldit")
+            }
+            cols[colI].textContent = value
+
+        }
+        resultsBody.appendChild(dataRow);
+    }
+}
